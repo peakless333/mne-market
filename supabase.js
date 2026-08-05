b87685fc9eb8c8025f30b82429d6caad526a16a8
peakless333 <peaklessworkingondying@gmail.com>
2026-08-05 03:54:29 +0200

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://kxrdujitnswuajtbdgar.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4cmR1aml0bnN3dWFqdGJkZ2FyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU4NjgxNzYsImV4cCI6MjEwMTQ0NDE3Nn0.17Gx37ZpxkJPNICCISTZHOdbflbmiV36NtORnWjCLOY';
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
